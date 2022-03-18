e66ee29324390819ac3275ef81b6269865a12d8c
Will Clark <bjclark@scidept.com>
2022-03-18 15:46:57 -0700

    Updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,8 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias ch_list='chamber list $(cat /var/tmp/interim.name)'
+alias ch_read='chamber read $(cat /var/tmp/interim.name)'
+alias ch_write='chamber write $(cat /var/tmp/interim.name)'
+alias ch_delete='chamber delete $(cat /var/tmp/interim.name)'
